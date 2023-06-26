4951aba205c5f54978ad19113c6bc7efed9401a6
Kelhann2608 <bernardin2608@gmail.com>
2023-06-26 23:09:39 +0200

    css modified

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,10 +11,10 @@ export default function Footer() {
     <footer className="max-w-3xl px-4 mx-auto sm:px-6 md:max-w-5xl">
       <hr className="w-full h-0.5 mx-auto mt-8 bg-or dark:bg-teal-500 border-0" />
       <div className="flex flex-col p-4 mx-auto text-center text-or dark:text-teal-500 md:flex-row md:justify-between">
-        <div className="flex flex-row items-center justify-center space-x-1 text-or dark:text-teal-500">
+        <div className="flex flex-row items-center justify-center space-x-1 text-xl text-or dark:text-teal-500">
           <span className="md:text-2xl">©️ {currentYear}</span>
           <span className="font-semibold font-DancingS text-or dark:text-teal-500 md:text-2xl">
-            Bernardin Milhet<a href="/" className="hover:underline"></a>
+            Bernardin Milhet
           </span>
         </div>
       </div>
